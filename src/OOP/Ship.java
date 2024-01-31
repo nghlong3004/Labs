c239656bd07baa2c6f7d629eb96a5d41bd01800e
nghlong3004 <nghlong3004@gmail.com>
2024-01-31 22:58:03 +0700

    update

---
+++ src/OOP/Ship.java
@@ -0,0 +1,23 @@
+package OOP;
+import java.util.ArrayList;
+public class Ship {
+	private ArrayList <String> locationCells;
+	private String name;
+	
+	
+	
+	public ArrayList<String> getLocationCells() {
+		return locationCells;
+	}
+	public void setLocationCells(ArrayList<String> locationCells) {
+		this.locationCells = locationCells;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
